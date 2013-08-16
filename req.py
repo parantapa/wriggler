b134930ad0dfa73bc30e91805088042713c5d9c9
Parantapa Bhattacharya <pb@parantapa.net>
2013-08-16 19:05:40 +0530

    Clean up the request wrapper

--- req.py
+++ req.py
@@ -31,10 +31,12 @@ def robust_http(args, kwargs, method):
         try:
             return method(*args, **kwargs)
         except requests.RequestException as e:
-            log.info(u"Try: {} - Get Failed: {}: {}", tries, type(e), e)
+            msg = u"Try L0: {} - Get Failed: {}: {}"
+            log.info(msg, tries, type(e), e)
             sleep(RETRY_AFTER)
         except Exception as e:
-            log.warn(u"Try: {} - Get Failed: {}: {}", tries, type(e), e)
+            msg = u"Try L0: {} - Get Failed: {}: {}"
+            log.warn(msg, tries, type(e), e)
             sleep(RETRY_AFTER)
 
     # Cant help any more; Quit program
@@ -47,27 +49,32 @@ def checked_http(args, kwargs, method):
     Check the status codes and repeat on server side errors.
     """
 
+    accept_codes = kwargs.pop("accept_codes", ())
+
     # Keep trying for until you get acceptable response
     for tries in xrange(RETRY_MAX):
         r = robust_http(args, kwargs, method)
 
         # Good response
-        if 200 <= r.status_code < 300:
+        if 200 <= r.status_code < 300 or r.status_code in accept_codes:
             return r
 
         # Bad client request, can't handle here
         if 400 <= r.status_code < 500:
-            log.info(u"Try: {}, Code {}: Client Error", tries, r.status_code)
+            msg = u"Try L1: {}, Code {}: Client Error: {}"
+            log.info(msg, tries, r.status_code, r.text)
             return r
 
         # Server problem retry
         if 500 <= r.status_code < 600:
-            log.info(u"Try: {}, Code {}: Server Error", tries, r.status_code)
+            msg = u"Try L1: {}, Code {}: Server Error: {}"
+            log.info(msg, tries, r.status_code, r.text)
             sleep(RETRY_AFTER)
             continue
 
         # Unknown response
-        log.warn(u"Try: {}, Code {}: Unexpected Error", tries, r.status_code)
+        msg = u"Try L1: {}, Code {}: Unexpected Error: {}"
+        log.warn(msg, tries, r.status_code, r.text)
         sleep(RETRY_AFTER)
         continue
 
@@ -81,12 +88,20 @@ def get(*args, **kwargs):
     Perform a robust GET request.
     """
 
-    return checked_http(args, kwargs, requests.get)
+    session = kwargs.get("session", None)
+    if session is None:
+        return checked_http(args, kwargs, requests.get)
+    else:
+        return checked_http(args, kwargs, session.get)
 
 def post(*args, **kwargs):
     """
     Perform a robust POST request.
     """
 
-    return checked_http(args, kwargs, requests.post)
+    session = kwargs.get("session", None)
+    if session is None:
+        return checked_http(args, kwargs, requests.post)
+    else:
+        return checked_http(args, kwargs, session.post)
 
