e7c3c216e767b12c0d2da18481d4165ef18d6f39
Parantapa Bhattacharya <pb@parantapa.net>
2013-12-09 20:57:17 +0530

    Partial cleanup of twitter code

---
+++ auth.py
@@ -0,0 +1,89 @@
+"""
+Defines the multi auth object for multiple key management.
+"""
+
+import sys
+import time
+import json
+
+import times
+
+RESET_BUFFER      = 5
+RATE_LIMIT_BUFFER = 1
+
+GIVE_UP_AFTER = 24 * 3600
+RETRY_AFTER   = 5
+RETRY_MAX     = GIVE_UP_AFTER // RETRY_AFTER
+WINDOW_TIME   = 15 * 60
+
+if __debug__:
+    from logbook import Logger
+    log = Logger(__name__)
+
+class MultiAuth(object):
+    """
+    Manage multiple twitter keys.
+    """
+
+    def __init__(self, keys):
+        super(MultiAuth, self).__init__()
+
+        now = times.to_unix(times.now())
+
+        self.idx    = 0
+        self.keys   = keys
+        self.remain = [sys.maxsize] * len(keys)
+        self.reset  = [now + WINDOW_TIME] * len(keys)
+
+    def get_token(self):
+        """
+        Get a token for the call.
+        """
+
+        return self.keys[self.idx]
+
+    def check_limit(self, headers):
+        """
+        Check if rate limit is hit for the current key.
+        """
+
+        now = times.to_unix(times.now())
+
+        try:
+            curtime = times.to_unix(times.to_universal(headers["date"]))
+            self.remain[self.idx] = int(headers["X-Rate-Limit-Remaining"])
+            self.reset[self.idx]  = int(headers["X-Rate-Limit-Reset"]) - curtime
+        except KeyError:
+            self.remain[self.idx] = 0
+            self.reset[self.idx]  = RETRY_AFTER
+
+        # Reset time in our system time
+        self.reset[self.idx] += now + RESET_BUFFER
+
+        # If we hit rate limit switch to the next key
+        if self.remain[self.idx] <= RATE_LIMIT_BUFFER:
+            if __debug__:
+                log.debug("Key {} hit rate limit ...", self.idx)
+            self.idx = (self.idx + 1) % len(self.keys)
+
+            # The next key had also hit rate limit previously
+            # Sleep off the rate limit window
+            if (self.remain[self.idx] <= RATE_LIMIT_BUFFER
+                    and self.reset[self.idx] <= now):
+                if __debug__:
+                    log.debug("Key {} still in rate limit ...", self.idx)
+                time.sleep(self.reset[self.idx] - now)
+
+def read_multi_auth(fname):
+    """
+    Read multiple keys from file.
+    """
+
+    if __debug__:
+        log.debug("Reading keys from {} ...", fname)
+
+    with open(fname) as fobj:
+        keys = json.load(fobj)
+
+    return MultiAuth(keys)
+
---
+++ rest.py
@@ -0,0 +1,142 @@
+"""
+Robust Twitter crawler primitives.
+"""
+
+from requests_oauthlib import OAuth1
+
+import pypb.req as req
+
+from logbook import Logger
+log = Logger(__name__)
+
+def list_to_csv(args):
+    """
+    Convert a list to a string csv.
+    """
+
+    args = map(str, args)
+    args = ",".join(args)
+    return args
+
+class Error(Exception):
+    """
+    Unrecoverable Error.
+    """
+
+class RetryExhausted(Error):
+    """
+    Maximum retry count exhausted.
+    """
+
+def twitter_rest_call(endpoint, auth, accept_codes, params):
+    """
+    Call a Twitter rest api endpoint.
+    """
+
+    auth = OAuth1(signature_type="auth_header", **auth.get_token())
+
+    tries = 0
+    while tries < RETRY_MAX:
+        r = req.get(endpoint, params=params, auth=auth, timeout=60.0)
+
+        # Proper receive
+        if 200 <= r.status_code < 300 or r.status_code in accept_codes:
+            auth.check_limit(r.headers)
+            return (r.json(), r.status_code)
+
+        # Check if rate limited
+        if r.status_code == 429:
+            log.info(u"Try {}: Being throttled - {} {}",
+                     tries, r.status_code, r.text)
+            auth.check_limit(r.headers)
+            tries += 1
+            continue
+
+        # Dont expect anything else
+        log.warn(u"Try {}: Unexepectd response - {} {}",
+                 tries, r.status_code, r.text)
+        auth.check_limit(r.headers)
+        tries += 1
+        continue
+
+    log.critical("Maximum retries exhausted ...")
+    raise RetryExhausted(endpoint, params)
+
+def users_show(auth, **params):
+    """
+    Return the information for a single user.
+    """
+
+    endpoint = "https://api.twitter.com/1.1/users/show.json"
+    accept_codes = (401, 403, 404)
+
+    params.setdefault("include_entities", 1)
+
+    return twitter_rest_call(endpoint, auth, accept_codes, params)
+
+def users_lookup(auth, **params):
+    """
+    Lookup profiles of as many users as possible.
+    """
+
+    endpoint = "https://api.twitter.com/1.1/users/lookup.json"
+    accept_codes = (403, 404)
+
+    params.setdefault("include_entities", 1)
+    if "user_id" in params:
+        params["user_id"] = list_to_csv(params["user_id"])
+    if "screen_name" in params:
+        params["screen_name"] = list_to_csv(params["screen_name"])
+
+    return twitter_rest_call(endpoint, auth, accept_codes, params)
+
+def statuses_user_timeline(auth, **params):
+    """
+    Get the user timeline of a single user.
+    """
+
+    endpoint = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    accept_codes = (403, 404)
+
+    params.setdefault("include_rts", 1)
+    params.setdefault("count", 200)
+
+    return twitter_rest_call(endpoint, auth, accept_codes, params)
+
+def friends_ids(auth, **params):
+    """
+    Get the friend ids of a given user.
+    """
+
+    endpoint = "https://api.twitter.com/1.1/friends/ids.json"
+    accept_codes = (403, 404)
+
+    params.setdefault("count", 5000)
+
+    return twitter_rest_call(endpoint, auth, accept_codes, params)
+
+def followers_ids(auth, **params):
+    """
+    Get the friend ids of a given user.
+    """
+
+    endpoint = "https://api.twitter.com/1.1/followers/ids.json"
+    accept_codes = (403, 404)
+
+    params.setdefault("count", 5000)
+
+    return twitter_rest_call(endpoint, auth, accept_codes, params)
+
+def search_tweets(auth, **params):
+    """
+    Search twitter for tweets.
+    """
+
+    endpoint = 'https://api.twitter.com/1.1/search/tweets.json'
+    accept_codes = ()
+
+    params.setdefault("count", 100)
+    params.setdefault("include_entities", "true")
+
+    return twitter_rest_call(endpoint, auth, accept_codes, params)
+
