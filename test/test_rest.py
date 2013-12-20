32af448741297206cc29510e41c3fdf14c38f017
Parantapa Bhattacharya <pb@parantapa.net>
2013-12-20 18:43:31 +0530

    Add test for statuses_user_timeline

--- test/test_rest.py
+++ test/test_rest.py
@@ -7,6 +7,11 @@ import pytest
 import twitter.auth as auth
 import twitter.rest as rest
 
+U = [
+    (813286, "BarackObama"),
+    (145125358, "SrBachchan")
+]
+
 @pytest.fixture
 def samp_auth():
     """
@@ -22,25 +27,25 @@ def test_users_show(samp_auth):
     Test the users_show method.
     """
 
-    params = {"user_id": 813286}
+    params = {"user_id": U[0][0]}
     profile, meta = rest.users_show(samp_auth, **params)
     assert meta["code"] == 200
-    assert profile["id"] == 813286
-    assert profile["screen_name"] == "BarackObama"
+    assert profile["id"] == U[0][0]
+    assert profile["screen_name"] == U[0][1]
 
-    params = {"screen_name": "BarackObama"}
+    params = {"screen_name": U[0][1]}
     profile, meta = rest.users_show(samp_auth, **params)
     assert meta["code"] == 200
-    assert profile["id"] == 813286
-    assert profile["screen_name"] == "BarackObama"
+    assert profile["id"] == U[0][0]
+    assert profile["screen_name"] == U[0][1]
 
 def test_users_lookup(samp_auth):
     """
     Test the users_lookup method.
     """
 
-    user_ids = set([813286, 145125358])
-    screen_names = set(["BarackObama", "SrBachchan"])
+    user_ids = set(u[0] for u in U)
+    screen_names = set(u[1] for u in U)
 
     params = {"user_id": list(user_ids)}
     profiles, meta = rest.users_lookup(samp_auth, **params)
@@ -53,4 +58,21 @@ def test_users_lookup(samp_auth):
     assert meta["code"] == 200
     assert user_ids == set(p["id"] for p in profiles)
     assert screen_names == set(p["screen_name"] for p in profiles)
+
+def test_statuses_user_timeline(samp_auth):
+    """
+    Test the statuses/user_timeline method.
+    """
+
+    params = {"user_id": U[0][0]}
+    tweets, meta = rest.statuses_user_timeline(samp_auth, **params)
+    assert meta["code"] == 200
+    assert all(t["user"]["id"] == U[0][0] for t in tweets)
+    assert all(t["user"]["screen_name"] == U[0][1] for t in tweets)
+
+    params = {"screen_name": U[0][1]}
+    tweets, meta = rest.statuses_user_timeline(samp_auth, **params)
+    assert meta["code"] == 200
+    assert all(t["user"]["id"] == U[0][0] for t in tweets)
+    assert all(t["user"]["screen_name"] == U[0][1] for t in tweets)
 
