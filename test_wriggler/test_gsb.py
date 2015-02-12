9daf1fdc5aed7abeeda8ebc804fe90a45012d800
Parantapa Bhattacharya <pb@parantapa.net>
2015-02-12 12:00:24 +0530

    Fixt gsb test case
    gsb.lookup now yields instead of returning

--- test_wriggler/test_gsb.py
+++ test_wriggler/test_gsb.py
@@ -28,8 +28,8 @@ def test_lookup(samp_key):
     Test the google safe browsing lookup function.
     """
 
-    response, status_code = gsb.lookup(samp_key, URLS)
-    assert status_code in (200, 204)
-    for u in URLS:
-        assert response[u] in ("phishing", "malware", "phishing,malware", "ok")
+    for response, status_code in gsb.lookup(samp_key, URLS):
+        assert status_code in (200, 204)
+        for u in URLS:
+            assert response[u] in ("phishing", "malware", "phishing,malware", "ok")
 
