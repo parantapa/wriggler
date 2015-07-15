592d14926a73cf95c952e375793b37abbac2e95d
Parantapa Bhattacharya <pb@parantapa.net>
2015-07-15 18:06:43 +0530

    Move list_to_csv out of twitter/rest.py

--- wriggler/twitter/rest.py
+++ wriggler/twitter/rest.py
@@ -10,15 +10,7 @@ from wriggler import log, Error
 import wriggler.const as const
 import wriggler.req as req
 import wriggler.twitter.error_codes as ec
-
-def list_to_csv(args):
-    """
-    Convert a list to a string csv.
-    """
-
-    args = map(str, args)
-    args = ",".join(args)
-    return args
+from wriggler.twitter import list_to_csv
 
 class TwitterRestAPIError(Error):
     """
