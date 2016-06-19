27573a93a0bb2327c12da38c1d99dc17ed69ffe2
Parantapa Bhattacharya <pb@parantapa.net>
2016-06-19 14:28:48 +0530

    Reduce the retry constants to 1

--- wriggler/const.py
+++ wriggler/const.py
@@ -3,17 +3,17 @@ Runtime constants.
 """
 
 # Retry on connection fail (seconds)
-CONNECT_RETRY_AFTER = 10
+CONNECT_RETRY_AFTER = 1
 
 # Maximum number of connection retries
-CONNECT_RETRY_MAX = 10
+CONNECT_RETRY_MAX = 1
 
 # In case of unknown error in api, retry after this many seconds
-API_RETRY_AFTER = 5
+API_RETRY_AFTER = 1
 
 # Maximum number of api retries
-API_RETRY_MAX = 10
+API_RETRY_MAX = 1
 
 # In case ratelimit is hit, wake 5 secs after the reset time is over.
-API_RESET_BUFFER = 5
+API_RESET_BUFFER = 1
 
